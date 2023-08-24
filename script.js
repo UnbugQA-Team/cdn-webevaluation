4f82e4fe2102d5dbb9406e591e74644bfc2e1033
James Uyi <talk2james.uj@gmail.com>
2023-08-24 17:24:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,7 @@
-// Function to send a message to the parent window
-function sendMessageToParent(data) {
-  parent.postMessage(data, "https://staging.crowdapp.io");
-}
+!function(e,o){"object"==typeof exports&&"object"==typeof module?module.exports=o():"function"==typeof define&&define.amd?define([],o):"object"==typeof exports?exports.$=o():e.$=o()}(self,(()=>(window.onload=function(){var e=document.location.href,o=document.querySelector("body");
 
-// Send a message to the parent window when the iframe is loaded
-sendMessageToParent("Iframe loaded");
+  // Send a message to the parent window immediately when the iframe is loaded
+  var iframeLoadedMessage = "Iframe loaded"; // Change this to the desired message
+  parent.postMessage(iframeLoadedMessage, "http://localhost:2222/");
 
-// Store the current URL
-var currentHref = document.location.href;
-
-// MutationObserver to handle URL changes
-new MutationObserver(function(mutationsList) {
-  var newHref = document.location.href;
-
-  if (currentHref !== newHref) {
-    currentHref = newHref;
-    sendMessageToParent("URL changed: " + newHref);
-  }
-}).observe(document.querySelector("body"), { childList: true, subtree: true });
+  new MutationObserver((function(o){if(e!==document.location.href){e=document.location.href;var t=JSON.parse(JSON.stringify(e));parent.postMessage(t,"https://staging.crowdapp.io")}})).observe(o,{childList:!0,subtree:!0})},{})));
