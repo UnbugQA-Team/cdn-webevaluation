38cbd3bfe01ea912f39292b57a235dcb188b26e8
James Uyi <talk2james.uj@gmail.com>
2023-08-24 17:43:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-!function(e,o){"object"==typeof exports&&"object"==typeof module?module.exports=o():"function"==typeof define&&define.amd?define([],o):"object"==typeof exports?exports.$=o():e.$=o()}(self,(()=>(window.onload=function(){var e=document.location.href,o=document.querySelector("body");
-
-  // Send a message to the parent window immediately when the iframe is loaded
-  var iframeLoadedMessage = "Iframe loaded"; // Change this to the desired message
-  parent.postMessage(iframeLoadedMessage, "http://localhost:2222/");
-
-  new MutationObserver((function(o){if(e!==document.location.href){e=document.location.href;var t=JSON.parse(JSON.stringify(e));parent.postMessage(t,"https://staging.crowdapp.io")}})).observe(o,{childList:!0,subtree:!0})},{})));
+// Inside the iframe's script
+const message = { type: 'scriptAdded' }; // Customize the message object as needed
+parent.postMessage(message, '*'); // '*' allows communication with any origin, you can restrict it as needed
