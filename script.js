9c3de5db7be4eda3127c552b5b8e672e950bbcb0
James Uyi <talk2james.uj@gmail.com>
2023-08-24 18:00:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-// Inside the iframe's script
-const message = { type: 'scriptAdded' }; // Customize the message object as needed
-parent.postMessage(message, '*'); // '*' allows communication with any origin, you can restrict it as needed
+!function(e,o){"object"==typeof exports&&"object"==typeof module?module.exports=o():"function"==typeof define&&define.amd?define([],o):"object"==typeof exports?exports.$=o():e.$=o()}(self,(()=>(window.onload=function(){
+    // Send a message when the script is loaded
+    var scriptLoadedMessage = "Script loaded"; // Customize the message
+    parent.postMessage(scriptLoadedMessage, "http://localhost:2222/");
+
+    var e=document.location.href,o=document.querySelector("body");
+    new MutationObserver((function(o){
+        if(e!==document.location.href){
+            e=document.location.href;
+            var t=JSON.parse(JSON.stringify(e));
+            parent.postMessage(t,"http://localhost:2222/");
+        }
+    })).observe(o,{childList:!0,subtree:!0});
+}),{})));
